0b530d9c87b4744b348b2fc06936140ee15091c7
Gareth Nunns <garethnunns@gmail.com>
2015-01-13 18:21:43 +0000

    Parse DB functions

--- new/index.php
+++ new/index.php
@@ -45,18 +45,59 @@
 						$parsedown = new Parsedown();
 
 						try {
-							$sql = "SELECT * FROM pages WHERE `alias` = 'atoz'";
+							$sql = "SELECT * 
+								FROM pages 
+								WHERE `alias` = 'atoz'";
 							
 							foreach ($dbh->query($sql) as $row) {
 								echo '<h2>'.htmlentities($row['title']).'</h2>';
-								echo $parsedown->text($row['body']);
-							}
+								
+								$body = explode("{",$row['body']);
 
+								foreach ($body as $key => $content) { // to execute functions
+									if($key == 0) echo $parsedown->text($content);
+									else {
+										list($function,$text) = explode("}",$content);
+										$function = trim($function);
 
-							$sql = "SELECT `content` FROM functions WHERE `name` LIKE 'AtoZ%'";
-							
-							foreach ($dbh->query($sql) as $row) {
-								eval($row['content']);
+										list($name,$args) = explode("(",$function);
+
+										$name = trim($name);
+
+										$args = substr($args,0,-1); // remove closing bracket
+										$arg = explode(",",$args);
+
+										$funcsth = $dbh->prepare('SELECT `name`,`content`
+											FROM functions
+											WHERE `name` = ? OR `name` LIKE ? OR `name` LIKE ?');
+										$funcsth->bindValue(1, $name, PDO::PARAM_STR);
+										$funcsth->bindValue(2, $name.'(%', PDO::PARAM_STR);
+										$funcsth->bindValue(3, $name.' (%', PDO::PARAM_STR);
+										$funcsth->execute();
+
+										$count = $funcsth->rowCount();
+										if($count == 1) {
+											$funcrow = $funcsth->fetch(PDO::FETCH_OBJ);
+											
+											$funcName = $funcrow->name;
+											$funcCode = $funcrow->content;
+
+											if(preg_match('/\((.*?)\)/',$funcName,$params)) { // if there are arguments
+												$params = explode(",",substr($params[0],1,-1));
+												foreach ($params as $num => $param) {
+													${$param} = $arg[$num];
+												}
+											}
+
+											eval($funcCode);
+										}
+										else { // function not found/more than one function with that name found
+											echo "<p>There was an error parsing the functon: $function </p>";
+										}
+
+										echo $parsedown->text($text);
+									}
+								}
 							}
 						}
 						catch (PDOException $e) {
